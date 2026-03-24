0fe99f40a48eb542cb63bea8ae29d6a38a53c452
Sploitwithme <rahulhiwale720@gmail.com>
2026-03-24 20:05:42 +0530

    fixed mongoose options

--- server/server.js
+++ server/server.js
@@ -18,12 +18,9 @@ app.use(cors({
 app.use(express.json());
 
 // MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log("MongoDB Connected"))
-.catch(err => console.log("Mongo Error:", err));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log("Mongo Error:", err));
 
 // Model
 const Prompt = mongoose.model("Prompt", {
